8af3f5f774c3f34f1736f6e575f24341ea35d399
Kolby <kolbykalafut@gmail.com>
2017-04-29 00:58:57 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Kolby',
-    text: 'Hey, what is up?',
-    createdAt: 123
-  });
-
   socket.on('createMessage', (message) => {
     console.log('Got a message from the client', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
